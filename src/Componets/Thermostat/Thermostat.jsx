b553e024587e1cfbd588e469542136ba4934aa32
jettison200 <jettison200@gmail.com>
2022-07-08 09:55:27 -0600

    refactor: removed the strict equality for 15

--- src/Componets/Thermostat/Thermostat.jsx
+++ src/Componets/Thermostat/Thermostat.jsx
@@ -1,83 +1,85 @@
-import React from 'react'
-import { useState } from 'react'
-import './Thermostat.css'
+import React from "react";
+import { useState } from "react";
+import "./Thermostat.css";
 
 function Thermostat() {
+  const [currentTemperature, setCurrentTemperature] = useState(15);
+  const [temparatureColor, setCurrentTemparatureColor] = useState("normal");
 
-    const [currentTemperature, setCurrentTemperature] = useState(15);
-    const [temparatureColor, setCurrentTemparatureColor] = useState("normal");
+  const increaseTemperature = () => {
+    const newTemperature = currentTemperature + 1;
 
-    const increaseTemperature = () => {
-        const newTemperature = currentTemperature + 1;
+    if (newTemperature > 25) return;
 
-        if (newTemperature > 25) return;
-
-        if (newTemperature === 15) {
-            setCurrentTemparatureColor("normal")
-        }
-
-        if (newTemperature > 5) {
-            setCurrentTemparatureColor("cool")
-        }
-
-        if (newTemperature > 10) {
-            setCurrentTemparatureColor("normal")
-        }
-
-        if (newTemperature > 17) {
-            setCurrentTemparatureColor("warm")
-        }
-
-        if (newTemperature > 20) {
-            setCurrentTemparatureColor("hot")
-        }
-
-
-        setCurrentTemperature(newTemperature);
+    if (newTemperature > 5) {
+      setCurrentTemparatureColor("cool");
     }
 
-    const decreaseTemperature = () => {
-        const newTemperature = currentTemperature - 1;
-
-        if (newTemperature < 5) return;
-        
-
-        if (newTemperature === 15) {
-            setCurrentTemparatureColor("normal")
-        } 
-
-        if (newTemperature < 20) {
-            setCurrentTemparatureColor("warm")
-        } 
-
-        if (newTemperature < 17) {
-            setCurrentTemparatureColor("normal")
-        } 
-
-        if (newTemperature < 10) {
-            setCurrentTemparatureColor("cool")
-        } 
-
-        if (newTemperature <= 5) {
-            setCurrentTemparatureColor("cold")
-        } 
-
-        setCurrentTemperature(newTemperature);
+    if (newTemperature > 10) {
+      setCurrentTemparatureColor("normal");
     }
 
+    if (newTemperature > 17) {
+      setCurrentTemparatureColor("warm");
+    }
+
+    if (newTemperature > 20) {
+      setCurrentTemparatureColor("hot");
+    }
+
+    setCurrentTemperature(newTemperature);
+  };
+
+  const decreaseTemperature = () => {
+    const newTemperature = currentTemperature - 1;
+
+    if (newTemperature < 5) return;
+
+    if (newTemperature === 15) {
+      setCurrentTemparatureColor("normal");
+    }
+
+    if (newTemperature < 20) {
+      setCurrentTemparatureColor("warm");
+    }
+
+    if (newTemperature < 17) {
+      setCurrentTemparatureColor("normal");
+    }
+
+    if (newTemperature < 10) {
+      setCurrentTemparatureColor("cool");
+    }
+
+    if (newTemperature <= 5) {
+      setCurrentTemparatureColor("cold");
+    }
+
+    setCurrentTemperature(newTemperature);
+  };
 
   return (
     <div className="Thermostat-Container">
-        <div className={`Display ${temparatureColor}`}>
-            <p id="Display-Text">{currentTemperature}°C</p>
-        </div>
+      <div className={`Display ${temparatureColor}`}>
+        <p id="Display-Text">{currentTemperature}°C</p>
+      </div>
 
-        <div className="Control-Button-Container">
-            <button onClick={() => increaseTemperature()} className="Control-Button">+</button>
-            <button onClick={() => decreaseTemperature()} className="Control-Button">-</button>
-        </div>
+      <div className="Control-Button-Container">
+        <button
+          onClick={() => increaseTemperature()}
+          className="Control-Button"
+        >
+          +
+        </button>
+        <button
+          onClick={() => decreaseTemperature()}
+          className="Control-Button"
+        >
+          -
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Thermostat
+export default Thermostat;
